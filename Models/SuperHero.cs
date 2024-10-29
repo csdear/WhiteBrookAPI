70dfd90f92fc1cda9a77be754f3d44f057056e6b
Stuart Dear <csdear138@gmail.com>
2024-10-29 14:37:54 -0500

    init, CRUD w/o DB

---
+++ Models/SuperHero.cs
@@ -0,0 +1,33 @@
+﻿namespace SuperHeroAPI.Models
+{
+    /// <summary>
+    /// Represents a Superhero.
+    /// </summary>
+    public class SuperHero
+    {
+        /// <summary>
+        /// Gets or sets the ID of the Superhero.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the Superhero.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the first name of the Superhero.
+        /// </summary>
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the last name of the Superhero.
+        /// </summary>
+        public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the place associated with the Superhero.
+        /// </summary>
+        public string Place { get; set; } = string.Empty;
+    }
+}
