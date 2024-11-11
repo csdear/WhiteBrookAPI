b36cae46fc0de7b101aff3494ff1c24de1cd84db
Stuart Dear <csdear138@gmail.com>
2024-11-11 05:51:59 -0600

    dockerize project. 5000, 5001

--- Program.cs
+++ Program.cs
@@ -17,10 +17,19 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SuperHeroAPI V1");
+        c.RoutePrefix = string.Empty;  // Serves Swagger UI at the root If you want Swagger to be available at /swagger instead of the root, change the RoutePrefix: c.RoutePrefix = "swagger";With this, Swagger UI would be accessible at http://localhost:5000/swagger or https://localhost:5001/swagger
+    });
 }
 
 app.UseHttpsRedirection();
